567ce0a0ef27caf3a14bee4ee2b8570de261bc7c
pridhvi15 <pridhvipradeep15@gmail.com>
2025-06-19 18:30:40 +0000

    content edited

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,9 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Pridhvi  Pradeep</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <p>Projects</p>
       </div>
     </>
   )
